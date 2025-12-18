8e39e9e751c223ead2c2693bf0ad1ae5f9c16a17
cranberryandmixnuts <sujin83668252@gmail.com>
2025-12-18 22:18:15 +0900

    asdfggh
    wqertgthfyjghk

--- Assets/File_cogus/Scripts/UI/SoundStorage.cs
+++ Assets/File_cogus/Scripts/UI/SoundStorage.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class SoundStorage : MonoBehaviour
 {
+    public static SoundStorage Instance { get; private set; }
 
+    [Header("BGM")]
+    AudioSource BossBGM;
+    AudioSource EndBGM;
+    AudioSource NomalBGM;
+    AudioSource CutsceneBGM;
+    AudioSource TitleBGM;
+
+    //[Header("SFX")]
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 }
